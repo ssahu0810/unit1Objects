fa739c2240ae855e20a61d2f44644df7717ad7e4
ssahu0810 <ssahu0810@gmail.com>
2015-09-01 10:30:29 -0500

    Messed around with colors and random function

--- Chapter 2 Practice/BrighterDemo.java
+++ Chapter 2 Practice/BrighterDemo.java
@@ -1,10 +1,32 @@
 import java.awt.Color;
 import javax.swing.JFrame;
+import java.util.Random;
 
 public class BrighterDemo
 {
     public static void main(String[] args)
     {
         JFrame frame = new JFrame();
-        frame.serSize(200,200);
-        Color mycolor = ...;
+        frame.setSize(1920,1080);
+        
+        Random generator = new Random();
+        
+        int count = 1;
+        while (count<2){
+            int n1 = generator.nextInt(256);
+            int n2 = generator.nextInt(256);
+            int n3 = generator.nextInt(256);
+            Color myColor = new Color(n1, n2, n3);
+            
+        
+            frame.getContentPane().setBackground(myColor);
+            frame.setVisible(true);
+        }
+        
+        
+        
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+}
